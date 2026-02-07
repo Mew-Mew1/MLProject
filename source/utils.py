cb8fb146c5c03007faf742b409486259df2a2079
Mew-Mew1 <adityarajsinghdigari@gmail.com>
2026-02-07 11:34:34 +0530

    Found the best model

--- source/utils.py
+++ source/utils.py
@@ -7,7 +7,8 @@ import dill
 
 import numpy as np
 import pandas as pd
-
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from source.exception import CustomException
 
 def save_object(file_path, obj):
@@ -18,5 +19,29 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, X_test, y_train, y_test, models, params):
+    try:
+        report = {}
+        for i in len(list(models.values())):
+            model = list(models.values())[i]
+            gs = GridSearchCV()
+            para = params[list(models.keys())[i]]
+
+            gs.fit(X_train, y_train)
+            model.set_param(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_pred = model.predict(X_test)
+
+            test_model_score = r2_score(y_test, y_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
     except Exception as e:
         raise CustomException(e, sys)
